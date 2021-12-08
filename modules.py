15a9287674188cd549ac1dcf83a9ad06ac50398e
Eric Chandler <echandler@uchicago.edu>
2021-12-08 17:00:02 -0600

    debug indexing

--- modules.py
+++ modules.py
@@ -217,16 +217,16 @@ class VectorQuantizedVAE(nn.Module):
         forward_seq_lin = forward_seq.reshape((forward_seq.shape[0],
                                             forward_seq.shape[1]*forward_seq.shape[2],
                                             forward_seq.shape[3]))
-        logger.debug(f" forward_seq shape {forward_seq_lin.shape}")
-        logger.debug(f" hidden shape {hidden.shape}")
+        # logger.debug(f" forward_seq shape {forward_seq_lin.shape}")
+        # logger.debug(f" hidden shape {hidden.shape}")
         # output.shape == B x D*D x H
         # hidden.shape == 1 x B x H
-        if forward_seq_lin.shape[0] == 0:
-            logger.error(f"RNN input has 0 length: {forward_seq_lin.shape}")
-        elif hidden.shape[0] == 0:
-            logger.error(f"Hidden RNN has 0 length: {hidden.shape}")
+        # if forward_seq_lin.shape[0] == 0:
+        #     logger.error(f"RNN input has 0 length: {forward_seq_lin.shape}")
+        # elif hidden.shape[0] == 0:
+        #     logger.error(f"Hidden RNN has 0 length: {hidden.shape}")
         output, hidden = self.gru(forward_seq_lin, hidden)
-        print(f"DEBUG: output shape {output.shape}")
+        #logger.debug(f" output shape {output.shape}")
         #logger.debug(f" hiddenoutput shape {hidden.shape}")
         # c_t_.shape == B x H
         c_t_ = output[:, p_sample, :] 
